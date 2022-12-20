80145d0d8847181182f9528b6eadfebec91c9bd2
ardelvito <ardelvito8738@gmail.com>
2022-12-20 14:38:00 +0700

    bottom nav & fragment part 2

--- app/src/main/java/com/example/dailymemedigest/MainActivity.kt
+++ app/src/main/java/com/example/dailymemedigest/MainActivity.kt
@@ -2,30 +2,142 @@ package com.example.dailymemedigest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.login_page.*
 
 class MainActivity : AppCompatActivity() {
 
+    val fragHome: Fragment = HomeFragment()
+    val fragLeaderboard: Fragment = LeaderboardFragment()
+    val fragCreation: Fragment = CreationFragment()
+    val fragSetting: Fragment = SettingFragment()
+    val fragManager : FragmentManager = supportFragmentManager
+    var mainFragment : Fragment = fragHome
+
+    val fragmentList:ArrayList<Fragment> = ArrayList()
+
+
+
+    private lateinit var menu : Menu
+    private lateinit var menuItem : MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        switchFragment()
+
+//        changeFragment()
+
+
+        chooseFragment()
+        viewPager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback(){
+                //anonymous class & object
+                override fun onPageSelected(position: Int) {
+                    bottomNavigationView.selectedItemId =
+                        bottomNavigationView.menu.getItem(position).itemId
+                }
+
+            }
+        )
+
     }
 
-    private fun switchFragment(){
-        var fragment: Fragment?=null
 
-        fragment = HomeFragment()
 
-        val args = Bundle()
+    private fun chooseFragment(){
+        fragmentList.add(HomeFragment())
+        fragmentList.add(CreationFragment())
+        fragmentList.add(LeaderboardFragment())
+        fragmentList.add(SettingFragment())
 
-        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
+        val frameLayoutAdapter =FrameLayoutAdapter(this,  fragmentList)
+        viewPager.adapter = frameLayoutAdapter
+
+        bottomNavigationView.setOnItemSelectedListener{
+            if(it.itemId == R.id.navi_home){
+                viewPager.currentItem = 0
+                Log.d("Fragment", "Home")
+
+            }
+            else if (it.itemId == R.id.navi_creation){
+                viewPager.currentItem = 1
+                Log.d("Fragment", "Creation")
+
+            }
+            else if (it.itemId == R.id.navi_leaderboard){
+                viewPager.currentItem = 2
+                Log.d("Fragment", "Leaderboard")
+
+
+            }
+            else if (it.itemId == R.id.navi_settings){
+                viewPager.currentItem = 3
+                Log.d("Fragment", "Settings")
+                
+            }
+            else if(it.itemId == R.id.fab_meme){
+                Log.d("FAB", "Clicked")
+            }
+            true
+
+        }
+
     }
+    //force close kalau ganti fragment bolak-balik (kalau transaction di dalam R.id)
+    //fragment ketumpuk kalau semua transaction di awal
+//    private fun changeFragment(){
+//        fragManager.beginTransaction().add(R.id.fragmentContent, fragHome).show(fragHome).commit()
+//
+//
+//
+//        menu = bottomNavigationView.menu
+////        menuItem = menu.getItem(0)
+////        menuItem.isChecked = true
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener {
+//            item->
+//            when (item.itemId){
+//                R.id.navi_home->{
+//                    callFragment(0, fragHome)
+//                }
+//                R.id.navi_creation->{
+//                    fragManager.beginTransaction().add(R.id.fragmentContent, fragCreation).show(fragCreation).commit()
+//                    callFragment(1, fragCreation)
+//                }
+//                R.id.navi_leaderboard->{
+//                    fragManager.beginTransaction().add(R.id.fragmentContent, fragLeaderboard).show(fragLeaderboard).commit()
+//                    callFragment(2, fragLeaderboard)
+//                }
+//                R.id.navi_settings->{
+//                    fragManager.beginTransaction().add(R.id.fragmentContent, fragSetting).show(fragSetting).commit()
+//                    callFragment(3, fragSetting)
+//                }
+//            }
+//            false
+//        }
+//
+//
+//
+//    }
+//
+//    private fun callFragment(i:Int, frag: Fragment){
+//        menuItem = menu.getItem(i)
+//
+//
+//        fragManager.beginTransaction().hide(mainFragment).show(frag).commit()
+//        mainFragment = frag
+//    }
 }
+
+
+
