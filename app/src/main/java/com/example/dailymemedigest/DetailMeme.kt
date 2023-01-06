3ee0fead0ebb6b8a631d3ecc97598dfe61bd91ea
ardelvito <ardelvito8738@gmail.com>
2023-01-06 10:26:56 +0700

    Detaim Meme UI Part 1
    Belum selesai

--- app/src/main/java/com/example/dailymemedigest/DetailMeme.kt
+++ app/src/main/java/com/example/dailymemedigest/DetailMeme.kt
@@ -3,7 +3,7 @@ package com.example.dailymemedigest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class memeDetails : AppCompatActivity() {
+class DetailMeme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_meme)
