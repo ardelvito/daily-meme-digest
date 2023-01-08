a901316006bc66fa07df82508507430a3808c6a6
yanuaraa <yanuara12393@gmail.com>
2023-01-08 20:27:15 +0700

    update drawer pt 2

--- app/src/main/java/com/example/dailymemedigest/MainActivity.kt
+++ app/src/main/java/com/example/dailymemedigest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.dailymemedigest
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,11 +15,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.viewpager2.widget.ViewPager2
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.drawer_header.view.*
 import kotlinx.android.synthetic.main.drawer_layout.*
+import kotlinx.android.synthetic.main.fragment_setting.*
 import kotlinx.android.synthetic.main.login_page.*
+import org.json.JSONObject
+import java.util.HashMap
 
 class MainActivity : AppCompatActivity() {
 
@@ -66,6 +78,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Navigation Bar
+        var sharedName = "com.example.dailymemedigest"
+        var preferences: SharedPreferences = getSharedPreferences(sharedName, Context.MODE_PRIVATE)
+        val idUser = preferences.getInt(Login.SHARED_PLAYER_ID, 0)
+        getDataDrawer(idUser)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         var drawerToggle =
@@ -75,16 +91,83 @@ class MainActivity : AppCompatActivity() {
         drawerToggle.syncState()
 
         navViewD.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.itemAddNewMemes -> Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show()
-                R.id.itemHome -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-                R.id.itemMyCreation -> Toast.makeText(this, "MyCreation", Toast.LENGTH_SHORT).show()
-                R.id.itemLeaderboard -> Toast.makeText(this, "LDB", Toast.LENGTH_SHORT).show()
-                R.id.itemSetting -> Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show()
+            chooseFragment()
+            viewPager.currentItem = when(it.itemId) {
+//                R.id.itemAddNewMemes -> 0
+                R.id.itemHome -> 0
+                R.id.itemMyCreation -> 1
+                R.id.itemLeaderboard -> 2
+                R.id.itemSetting -> 3
+                else -> 0
             }
             drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
+
+        // Logout
+        val navViewDH = findViewById<NavigationView>(R.id.navViewD)
+        val DH:View = navViewDH.getHeaderView(0)
+        val logoutBtn:View = DH.findViewById(R.id.btnLogoutDH)
+        logoutBtn.setOnClickListener {
+            //create intent object & determine the activity target
+            val intent = Intent(this, Login::class.java)
+
+            //"execute" the object and OS will launch the activity target
+            startActivity(intent)
+        }
+    }
+
+    fun getDataDrawer(user_id: Int) {
+        val q = Volley.newRequestQueue(this)
+        val url = "https://ubaya.fun/native/160420024/memes_api/get_userprofile.php"
+
+        val stringRequest = object : StringRequest(
+            Request.Method.POST,
+            url, Response.Listener{
+                Log.d("Cek Parameter Settings", it.toString())
+                val obj = JSONObject(it)
+                val result = obj.getString("result")
+                if(result == "OK"){
+                    Log.d("Status", "Berhasil Ambil Data")
+                    val arrData = obj.getJSONArray("data")
+                    val userObj = arrData.getJSONObject(0)
+
+                    val firstName = userObj.getString("first_name")
+                    val lastName = userObj.getString("last_name")
+                    val avatarLink = userObj.getString("avatar_link")
+                    val username = userObj.getString("username")
+
+                    navViewD.txtUsernameDH.setText(username.toString())
+                    navViewD.txtFNameDH.setText(firstName.toString())
+                    navViewD.txtLNameDH.setText(lastName.toString())
+                    Picasso.get().load(avatarLink).into(navViewD.imgProfileDH)
+
+//                    txtUserName.setText(username.toString())
+//                    txtUserFullName.setText(firstName + " " + lastName)
+//                    Picasso.get().load(avatarLink).into(circleImgBorder)
+//                    txtInputFirstName.editText?.setText(firstName)
+//                    txtInputLastName.editText?.setText(lastName)
+
+                }
+            }
+
+            ,
+            {
+                Response.ErrorListener{
+                    Log.d("Cek Parameter", it.message.toString())
+                }
+            }
+        )
+        {
+            override fun getParams(): MutableMap<String, String>? {
+                val map = HashMap<String, String>()
+                map.set("user_id", user_id.toString())
+                Log.d("MAP", map.toString())
+                return map
+            }
+
+        }
+        q.add(stringRequest)
     }
 
     private fun chooseFragment(){
